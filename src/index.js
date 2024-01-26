7b0a48d41ed0227d2a134b202839a1d25c840d21
tachimo <92985370+tachimo@users.noreply.github.com>
2024-01-26 12:24:07 +0000

    Add:jsxの記述ルール確認

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<h1>こんにちは！</h1>);
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
+};
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
